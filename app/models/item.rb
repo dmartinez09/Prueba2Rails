89386801226ee63d6a25883838b67924e07f7508
Diego Martinez <dmartinezvergara09@gmail.com>
2017-07-19 19:59:51 -0400

    cuarto commit model scopes en item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
 	belong_to :category
 	belong_to :user, optional: true
+
+	scope :return_size, -> (size) { where("size=?", size)}
+	scope :user_unassigned, -> { where(user_id: nil) }
 end
