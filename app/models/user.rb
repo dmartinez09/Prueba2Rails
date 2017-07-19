ed8abceefa761fbc72e4a1c091d885e6276d5bf8
Diego Martinez <dmartinezvergara09@gmail.com>
2017-07-19 19:49:38 -0400

    tercer commit model user no relacionado

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
 	has_many :items, dependent: :destroy
 	validates :name, presence: :true
 	validates :email, uniqueness: true
+	before_save :inicial_mayuscula
+
+	def inicial_mayuscula
+		self.name.capitalize!
+		
+	end
 
 end
