386f8a7b538033c9c914a7e38b47c164ad40f5ae
Diego Martinez <dmartinezvergara09@gmail.com>
2017-07-19 20:04:09 -0400

    quinto commit model callback destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,14 @@ class Item < ApplicationRecord
 
 	scope :return_size, -> (size) { where("size=?", size)}
 	scope :user_unassigned, -> { where(user_id: nil) }
+
+	after_destroy :delete_category
+
+	def delete_category
+		Category.all.each do |x|
+			unless category.items.any?
+				category.destroy
+		
+	end
+end
 end
